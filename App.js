231c4cdb61077eab8b76e473664d774b5487363d
prakash <42199640+achilles24@users.noreply.github.com>
2024-07-05 13:20:59 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Video and Photo Lists</h1>
+      <div className="lists-container">
+        <VideoListWithData />
+        <PhotoListWithData />
+      </div>
+    </div>
+  );
+};
+
+export default App;
